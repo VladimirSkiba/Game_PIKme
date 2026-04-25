580603efbfd18647e6c492000a16e288ca8264cf
EgorMurzyakov <EgorM40720@gmail.com>
2026-04-25 20:06:11 +0400

    1.0.16.1
    Книги содержат заклинания (тип bool)

--- Assets/Game/Scripts/Inventory/Item/BookItem.cs
+++ Assets/Game/Scripts/Inventory/Item/BookItem.cs
@@ -1,12 +1,61 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Book Item", menuName = "Inventory/Items/New Book Item")]
 
 public class BookItem : ItemScriptableObject
 {
+    [SerializeField] private Spells magicSpells;
+
+    public override ItemScriptableObject Clone()
+    {
+        BookItem clone = ScriptableObject.CreateInstance<BookItem>();
+
+        clone.itemID = itemID;
+        clone.type = this.type;
+        clone.ItemPrefab = this.ItemPrefab;
+        clone.itemName = this.itemName;
+        clone.itemDescription = this.itemDescription;
+        clone.maximumAmount = this.maximumAmount;
+        clone.icon = this.icon;
+        clone.magicSpells = magicSpells;
+        return clone;
+    }
 
     public void Start()
     {
         type = ItemType.Book;
+    }
+
+    public Spells GetMagicSpells()
+    {
+        return magicSpells.Clone();
+    }
+}
+
+[System.Serializable]
+public class Spells
+{
+    public bool fireball;
+    public bool tornado;
+
+    public Spells Clone()
+    {
+        return new Spells(this);
+    }
+
+    public Spells(bool _fireball, bool _tornado)
+    {
+        this.fireball = _fireball;
+        this.tornado = _tornado;
+    }
+
+    public Spells(Spells _other)
+    {
+        if (_other != null)
+        {
+            this.fireball = _other.fireball;
+            this.tornado = _other.tornado;
+        }
     }
 }
