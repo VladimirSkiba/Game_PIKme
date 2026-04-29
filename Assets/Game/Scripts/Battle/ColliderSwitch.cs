36cd458ff741b9932aec1cd18ef4cb46b61838d4
EgorMurzyakov <EgorM40720@gmail.com>
2026-04-30 03:59:49 +0400

    1.0.17.2
    Доделал голема (почти). Осталось добавить звуки

--- Assets/Game/Scripts/Battle/ColliderSwitch.cs
+++ Assets/Game/Scripts/Battle/ColliderSwitch.cs
@@ -2,47 +2,37 @@ using System;
 using UnityEngine;
 using Unity.VisualScripting;
 
+public enum damageCollider { Weapon, Foot}
+
 public class ColliderSwitch : MonoBehaviour // Универсальный (и для игрока и для врагов)
 {
     private state currentState = state.Empty;
 
-    public event Action weaponColliderOn;
-    public event Action weaponColliderOff;
-    public event Action footColliderOn;
-    public event Action footColliderOff;
+    //public event Action weaponColliderOn;
+    //public event Action weaponColliderOff;
+    public event Action<damageCollider> weaponColliderOn;
+    public event Action<damageCollider> weaponColliderOff;   
 
     public void ChoosingAction(state _st)
     {
         currentState = _st;
         if (currentState != state.Attack) // При выходе с состояния атаки Принудительно выключаем коллайдер
         {
-            WeaponColliderOFF();
+            WeaponColliderOFF(damageCollider.Weapon);
+            WeaponColliderOFF(damageCollider.Foot);
         }
     }
 
-    public void WeaponColliderOn()
+    public void WeaponColliderOn(damageCollider _damCol)
     {
-        if (currentState == state.Attack) // Защита от случайного срабатывания (на случай, если состояние сменится, а Анимационное событие будет вызвано)
+        if (currentState == state.Attack || currentState == state.AttackB) // Защита от случайного срабатывания (на случай, если состояние сменится, а Анимационное событие будет вызвано)
         {
-            weaponColliderOn?.Invoke();
+            weaponColliderOn?.Invoke(_damCol);
         }
     }
 
-    public void WeaponColliderOFF()
+    public void WeaponColliderOFF(damageCollider _damCol)
     {
-        weaponColliderOff?.Invoke();
-    }
-
-    public void GolemFootColliderOn()
-    {
-        if (currentState == state.Attack) // Защита от случайного срабатывания (на случай, если состояние сменится, а Анимационное событие будет вызвано)
-        {
-            footColliderOn?.Invoke();
-        }
-    }
-
-    public void GolemFootColliderOff()
-    {
-        footColliderOff?.Invoke();
+        weaponColliderOff?.Invoke(_damCol);
     }
 }
